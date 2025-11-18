ac276c8f98427f3fda632fe0ee957c3e26389e98
orchid127 <ndougakimberley@gmail.com>
2025-11-18 14:34:40 +0100

    fixing navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,11 +16,11 @@ export default function Navbar () {
                 <span className="h-[0.1rem] w-full bg-[#9C80DB] rounded-full"></span>
                 <span className="h-[0.1rem] w-full bg-[#9C80DB] rounded-full"></span>
             </div>
-            <ul className={`${menuOpen ? "flex" : "hidden"} sm:flex flex-col sm:flex-row items-center w-[100%] sm:w-auto sm:space-x-8 mb-[0.25rem] sm:mb-0 m-0`}>
-                <li className='text-center list-none mb-[0.5em] sm:my-0 text-[1.5rem] sm:text-[1rem]'><a href="#about" className="!text-[#FAF5E9] hover:!text-[#9C80DB] !no-underline">About</a></li>
-                <li className='text-center list-none mb-[0.5em] sm:my-0 text-[1.5rem] sm:text-[1rem]'><a href="#projects" className="!text-[#FAF5E9] hover:!text-[#9C80DB] !no-underline">Projects</a></li>
-                <li className='text-center list-none mb-[0.5em] sm:my-0 text-[1.5rem] sm:text-[1rem]'><a href="#contact" className="!text-[#FAF5E9] hover:!text-[#9C80DB] !no-underline">Contact</a></li>
-                <li className='text-center list-none mb-[0.5em] text-[1.5rem] sm:text-[1rem]'><Button title="Resume" link="/resume.pdf"/></li>
+            <ul className={`${menuOpen ? "flex" : "hidden"} sm:flex flex-col sm:flex-row items-start sm:items-center w-[100%] sm:w-auto sm:space-x-8 mb-[0.25rem] sm:mb-0 m-0`}>
+                <li className='text-center list-none mb-[0.5em] sm:mb-0 text-[1.5rem] sm:text-[1rem]'><a href="#about" className="!text-[#FAF5E9] hover:!text-[#9C80DB] !no-underline">About</a></li>
+                <li className='text-center list-none mb-[0.5em] sm:mb-0 text-[1.5rem] sm:text-[1rem]'><a href="#projects" className="!text-[#FAF5E9] hover:!text-[#9C80DB] !no-underline">Projects</a></li>
+                <li className='text-center list-none mb-[0.5em] sm:mb-0 text-[1.5rem] sm:text-[1rem]'><a href="#contact" className="!text-[#FAF5E9] hover:!text-[#9C80DB] !no-underline">Contact</a></li>
+                <li className='text-center list-none mb-[0.5em] sm:mb-0 text-[1.5rem] sm:text-[1rem]'><Button title="Resume" link="/resume.pdf"/></li>
             </ul>
         </nav>
     );
